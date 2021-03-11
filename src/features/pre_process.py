38a223fe4c89b57a8a65a4aefb55e32633e2e9d7
Stephen Betts <stevo.betts@gmail.com>
2021-03-11 17:51:03 +0000

    extra commenting

--- src/features/pre_process.py
+++ src/features/pre_process.py
@@ -6,21 +6,25 @@ from nltk.corpus import stopwords
 import gensim
 
 def set_stop_words(extra_words):
+     """Top up the nltk supplied stopword list with additional words"""
      stop_words = stopwords.words('english')
      stop_words.extend(extra_words)
      return stop_words
 
 
 def sent_to_words(sentences):
+    """ Converts a document into a list of lowercase tokens, removing accent marks"""
     for sentence in sentences:
         yield (simple_preprocess(str(sentence), deacc=True))
 
 
 def remove_stopwords(texts, added_stop_words):
+    """ Work through word list and drop items belonging to the stop list"""
     return [[word for word in simple_preprocess(str(doc)) if word not in set_stop_words(added_stop_words)] for doc in texts]
 
 
 def bigrams(words, bi_min=2, tri_min=10):
+    """ Grouping related phrases together into bigrams """
     bigram = gensim.models.Phrases(words, min_count=bi_min)
     bigram_mod = gensim.models.phrases.Phraser(bigram)
     return bigram_mod
@@ -28,7 +32,11 @@ def bigrams(words, bi_min=2, tri_min=10):
 
 def get_corpus(df, added_stop_words):
     """
-    Get Bigram Model, Corpus, id2word mapping
+    Call the pre-processing functions.
+    The dictionary class that is supplied the bigram will output
+    the word freq count of each word in each review
+     - doc2bow() to create corpus counts the no of occurrences of each distinct word, converts the word
+       to its integer word id and returns the result as a sparse vector.
     """
     words = list(sent_to_words(df.text))
     words = remove_stopwords(words, added_stop_words)
@@ -39,3 +47,15 @@ def get_corpus(df, added_stop_words):
     id2word.compactify()
     corpus = [id2word.doc2bow(text) for text in bigram]
     return corpus, id2word, bigram
+
+
+def get_test_bigram(df, added_stop_words):
+    """
+    For the test data we only need the bigram data built on the TEST reviews,
+    using the TRAIN id2word mappings.
+    """
+    words = list(sent_to_words(df.text))
+    words = remove_stopwords(words, added_stop_words)
+    bigram = bigrams(words)
+    bigram = [bigram[review] for review in words]
+    return bigram
--- src/models/text_model.py
+++ src/models/text_model.py
@@ -3,30 +3,31 @@ import warnings
 import gensim
 
 def gen_lda_model(corpus, id2word, model_log, model_data):
+    """ Constructor that estimates Latent Dirichlet Allocation model parameters
+    based on the supplied training corpus"""
     logging.basicConfig(filename=model_log, format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
     with warnings.catch_warnings():
         warnings.simplefilter('ignore')
         lda_train4 = gensim.models.ldamulticore.LdaMulticore(
                                corpus=corpus,
-                               num_topics=10,
+                               num_topics=20,
                                id2word=id2word,
                                chunksize=100,
                                workers=2, # Num. Processing Cores - 1
-                               passes=20,
-                               eval_every = 1,
+                               passes=10,
+                               #eval_every = 1,
                                per_word_topics=True)
         lda_train4.save(model_data)
         return lda_train4
 
-def train_vectors(lda_model, train_corp, df):
+def train_vectors(lda_model, train_corp, text_train_df):
+    """Create a feature vector for every review"""
     train_vecs = []
-    #for i in range(len(text_train)):
-    for i in range(1000):  # REVERT AFTER TEST
+    for i in range(len(text_train)):
         top_topics = lda_model.get_document_topics(train_corp[i], minimum_probability=0.0)
         topic_vec = [top_topics[i][1] for i in range(10)]
-        #topic_vec.extend([len(text_train_df.iloc[i].text)]) # length review
-        topic_vec.extend([len(df.iloc[i].text)]) # length review  # REVERT AFTER TEST
+        topic_vec.extend([len(text_train_df.iloc[i].text)]) # length review
         train_vecs.append(topic_vec)
     return train_vecs
 
